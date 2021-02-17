5b4becb8c6f00ffbd3cc14e5789f7910806bb401
Gabriel Figueredo <gfigueredo937@gmail.com>
2021-02-17 03:06:37 -0300

    Added project

---
+++ ripples/ripples.js
@@ -0,0 +1,30 @@
+$(".ripple-effect").on("pointerdown", function(e) {
+    let rect = this.getBoundingClientRect();
+    let radius = findFurthestPoint(e.clientX, this.offsetWidth, rect.left, e.clientY, this.offsetHeight, rect.top);
+
+    let circle =  document.createElement("span");
+    circle.classList.add("ripple");
+
+    circle.style.left = e.clientX - rect.left - radius + "px";
+    circle.style.top = e.clientY - rect.top - radius + "px";
+    circle.style.width = circle.style.height = radius * 2 + "px";
+
+    $(this).prepend(circle);
+});
+
+$(".ripple-effect").on("pointerup mouseleave dragleave touchend", function(e) {
+    let ripple = $(this).find(".ripple");
+    if (ripple.lenght != 0) {
+        ripple.css("opacity", "0");
+        setTimeout(() => {
+            ripple.remove()
+        }, 300);
+    }
+});
+
+function findFurthestPoint(clickPointX, elementWidth, offsetX, clickPointY, elementHeight, offsetY) {
+    let x = clickPointX - offsetX > elementWidth / 2 ? 0 : elementWidth;
+    let y = clickPointY - offsetY > elementHeight / 2 ? 0 : elementHeight;
+    let d = Math.hypot(x - (clickPointX - offsetX), y - (clickPointY - offsetY));
+    return d;
+}

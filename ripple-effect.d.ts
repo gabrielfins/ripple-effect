8bed61b43140c4f553ea68368f162102d1d520db
Gabriel Figueredo <contato.gabrielfins@gmail.com>
2026-09-17 23:18:30 -0300

    docs: add JSDoc comment to the ambient module declaration
    Gives editors a hover tooltip on `import '@gabrielfins/ripple-effect'`
    explaining that it's a side-effect-only import with no exports.

--- ripple-effect.d.ts
+++ ripple-effect.d.ts
@@ -1,1 +1,9 @@
+/**
+ * Adds a Material Design ripple effect to any element with a `data-ripples`
+ * attribute. Side-effect only: importing this module wires up the global
+ * pointer listeners, it has no exports.
+ *
+ * @example
+ * import '@gabrielfins/ripple-effect';
+ */
 declare module '@gabrielfins/ripple-effect';
